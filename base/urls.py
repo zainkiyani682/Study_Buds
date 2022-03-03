6f69baf8e60e7986335bf8036c13ce084fa93be6
Zain <zainkiyani682@gmail.com>
2022-03-03 20:04:54 +0500

    Study buds

---
+++ base/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from base import views
+urlpatterns = [
+    path('login', views.loginPage, name = "login"),
+    path('logout', views.logoutPage, name = "logout"),
+    path('register', views.registerUser, name = "register"),
+    path('profile/<str:pk>/', views.userProfile, name = "profile"),
+    
+    
+    path ('', views.home, name="home"),
+    path ('room/<str:pk>/', views.room, name="room"),
+    path('create-room',views.createRoom, name="create-room" ),
+    path('update-room/<str:pk>/',views.updateRoom, name="update-room" ),
+    path('delete-room/<str:pk>/',views.deleteRoom, name="delete-room" ),
+    path('delete-message/<str:pk>/',views.deleteMessage, name="delete-message" ),
+
+    path('update-user',views.updateUser, name="update-user" ),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+]
